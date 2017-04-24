acf9d4e6bd84cc9933287901974a8a75ee679352
Den <den@Ebon.Hawk>
2017-04-24 03:16:44 +0300

    cleanUp

--- src/test/java/com/dd/bdd/steps/AuthorizationSteps.java
+++ src/test/java/com/dd/bdd/steps/AuthorizationSteps.java
@@ -1,34 +1,26 @@
 package  com.dd.bdd.steps;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.PageFactory;
-
 import com.bdd.dd.helpers.UserData;
 import com.dd.bdd.page_Objects.FeedPage;
 import com.dd.bdd.page_Objects.LoginPage;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.concurrent.TimeUnit;
-
 import org.junit.Assert;
-
-
 
 public class AuthorizationSteps {
 
 	public WebDriver driver;
 	public LoginPage loginPage;
 	public FeedPage feedPage;
-	
-	
+		
 	@Given("^user is on the login page$")
 	public void user_is_on_the_login_page() throws InterruptedException {
 		DesiredCapabilities capabilities = new DesiredCapabilities();
@@ -58,6 +50,7 @@ public class AuthorizationSteps {
 	public void user_logs_in_using_his_phone_number()  {
 		loginPage.fillFormAndLogin(UserData.userPhone,UserData.userPassword);
 	}
+	
 	@When("^user logs in using wrong phone number$")
 	public void user_logs_in_using_wrong_phone_number()  {
 		loginPage.fillFormAndLogin(UserData.userPassword,UserData.userPassword);
@@ -67,14 +60,14 @@ public class AuthorizationSteps {
 	public void user_see_error_message() {
 		Assert.assertTrue(loginPage.isErrorMessageDisplayed());
 	}
+	
 	@Then("^user gets on the profile page$")
 	public void  user_gets_on_the_feed_page() throws Throwable {
-		
 		Assert.assertEquals("https://m.vk.com/feed", driver.getCurrentUrl());
 	}
+	
 	@Then("^browser closing$")
-	public void browser_closing() throws Throwable {
+	public void browser_closing(){
 		driver.quit();
-
 	}
 }
