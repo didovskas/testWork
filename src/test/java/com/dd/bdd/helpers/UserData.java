db87aaa2e45c8321a80d406a0b370117462a691e
didovskas <didovskas@gmail.com>
2017-04-26 23:10:36 +0300

    Delete UserData.java

--- src/test/java/com/dd/bdd/helpers/UserData.java
+++
@@ -1,9 +0,0 @@
-package com.dd.bdd.helpers;
-
-public class UserData {
-	
-	public static String userEmail = "didovskas@gmail.com";
-	public static String userPhone = "+380953579313";
-	public static String userPassword ="Banan&Nutela";
-
-}
